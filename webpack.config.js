9885ec60871192fcad9fe22ae6605c1efee91bc9
Samuel <gressboldude@gmail.com>
2019-03-20 18:02:36 +0100

    added redux boilerplate and changed the auth flow little bit, more to come

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/js/app.js',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'public/script/js'),
     filename: 'scripts.bundle.js'
